814eacd63e438d2e3c41bdcfa95668f6bcbceaf8
jerryZhou <jerry.zhou@ptn.advantest.com>
2021-04-22 19:36:25 +0800

    Add router view

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const appRoot = require('app-root-path');
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Used app root path
+global.$at = appRoot;
+global.$re = appRoot.require;
 
 // Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,9 +15,17 @@ app.use('/upload', express.static(path.join(__dirname, 'upload')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Hi node...')
-})
+// Used Initialize
+require('./middleware/initialize')(app);
+
+// Used reference
+app.use(require('./middleware/reference'));
+
+// Used API 
+require('./apis/base')(app);
+
+// Used View
+require('./routers/base')(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, async () => {
